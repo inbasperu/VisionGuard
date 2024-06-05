44885072fb0f1fb7c5857d69e7815f78059b34ba
Inbasekaran <inba.peru@gmail.com>
2024-06-05 11:18:10 +0530

    Added slots template ➕

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -12,3 +12,74 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionExit_triggered()
+{
+
+}
+
+void MainWindow::on_actionSettings_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_Landmarks_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_Head_Pose_Axes_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_Gaze_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_Face_Bounding_Box_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_Eye_State_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_All_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionShow_None_triggered()
+{
+
+}
+
+
+void MainWindow::on_Calibrate_clicked()
+{
+
+}
+
+
+void MainWindow::showGazeTime()
+{
+
+}
+
+
+void MainWindow::showGazeLostTime()
+{
+
+}
+
--- src/app/mainwindow.h
+++ src/app/mainwindow.h
@@ -17,6 +17,31 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionExit_triggered();
+
+    void on_actionShow_None_triggered();
+
+    void on_actionSettings_triggered();
+
+    void on_actionShow_Landmarks_triggered();
+
+    void on_actionShow_Head_Pose_Axes_triggered();
+
+    void on_actionShow_Gaze_triggered();
+
+    void on_actionShow_Face_Bounding_Box_triggered();
+
+    void on_actionShow_Eye_State_triggered();
+
+    void on_actionShow_All_triggered();
+
+    void on_Calibrate_clicked();
+
+    void showGazeTime();
+
+    void showGazeLostTime();
+
 private:
     Ui::MainWindow *ui;
 };
