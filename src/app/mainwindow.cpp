1116e2ca46101b9e91f09e99efb2d3957f1c8191
Inbasekaran <inba.peru@gmail.com>
2024-06-05 18:49:28 +0530

    Interfaced camera to the priview window 📷

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -1,85 +1,67 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTime>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  // visionGuard = new VisionGuard();
+  timer = new QTimer(this);
+
+  connect(ui->Calibrate, &QPushButton::clicked, this,
+          &MainWindow::on_Calibrate_clicked);
+  // connect(ui->startButton, &QPushButton::clicked, this,
+  // &MainWindow::on_startButton_clicked);
+  connect(timer, &QTimer::timeout, this, &MainWindow::updateFrame);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+  delete ui;
+  // delete visionGuard;
 }
 
-void MainWindow::on_actionExit_triggered()
-{
-
+void MainWindow::on_Calibrate_clicked() {
+  // visionGuard->calibrate();
+  if (!cap.isOpened()) {
+    cap.open(0); // Open default camera
+  }
+  timer->start(33); // 30 FPS
 }
 
-void MainWindow::on_actionSettings_triggered()
-{
-
+void MainWindow::on_startButton_clicked() {
+  if (!cap.isOpened()) {
+    cap.open(0); // Open default camera
+  }
+  timer->start(33); // 30 FPS
 }
 
+void MainWindow::updateFrame() {
+  cv::Mat frame;
+  cap >> frame; // Capture a new frame from the camera
 
-void MainWindow::on_actionShow_Landmarks_triggered()
-{
+  if (frame.empty())
+    return;
 
+  // visionGuard->processFrame(frame);
+
+  // Resize the frame to 700x428
+  cv::Mat resizedFrame;
+  cv::resize(frame, resizedFrame, cv::Size(700, 428));
+
+  // Convert the resized frame to QImage
+  cv::cvtColor(resizedFrame, resizedFrame,
+               cv::COLOR_BGR2RGB); // Convert BGR to RGB
+  QImage img =
+      QImage((const unsigned char *)(resizedFrame.data), resizedFrame.cols,
+             resizedFrame.rows, QImage::Format_RGB888);
+  ui->lastImagePreviewLabel->setPixmap(QPixmap::fromImage(img));
+
+  QTime time{QTime::currentTime()};
+  QString time_text{time.toString("mm : ss")};
+  ui->GazeTime->setText(time_text);
+  ui->GazeLostTime->setText("00 : 00");
+
+  // ui->GazeTime->setText(QString::number(visionGuard->getGazeTime()));
+  // ui->GazeLostTime->setText(QString::number(visionGuard->getGazeAwayTime()));
 }
-
-
-void MainWindow::on_actionShow_Head_Pose_Axes_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionShow_Gaze_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionShow_Face_Bounding_Box_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionShow_Eye_State_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionShow_All_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionShow_None_triggered()
-{
-
-}
-
-
-void MainWindow::on_Calibrate_clicked()
-{
-
-}
-
-
-void MainWindow::showGazeTime()
-{
-
-}
-
-
-void MainWindow::showGazeLostTime()
-{
-
-}
-
--- src/app/mainwindow.h
+++ src/app/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <opencv2/opencv.hpp>
+// #include "VisionGuard.h"  // Include your VisionGuard header
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,40 +12,23 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private slots:
-    void on_actionExit_triggered();
-
-    void on_actionShow_None_triggered();
-
-    void on_actionSettings_triggered();
-
-    void on_actionShow_Landmarks_triggered();
-
-    void on_actionShow_Head_Pose_Axes_triggered();
-
-    void on_actionShow_Gaze_triggered();
-
-    void on_actionShow_Face_Bounding_Box_triggered();
-
-    void on_actionShow_Eye_State_triggered();
-
-    void on_actionShow_All_triggered();
-
-    void on_Calibrate_clicked();
-
-    void showGazeTime();
-
-    void showGazeLostTime();
+  void on_Calibrate_clicked();
+  void on_startButton_clicked();
+  void updateFrame();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
+  // VisionGuard *visionGuard;  // Your VisionGuard object
+  QTimer *timer;
+  cv::VideoCapture cap;
 };
+
 #endif // MAINWINDOW_H
